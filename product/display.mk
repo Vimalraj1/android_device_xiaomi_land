afc3a5592790183e98baa626a6cfc0b3954dcea5
soubhik-khan <soubhik.khan@gmail.com>
2017-06-02 23:27:17 +0530

    land: build omni offline charger

--- product/display.mk
+++ product/display.mk
@@ -23,3 +23,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
+
+# OMNI Charger Images
+PRODUCT_PACKAGES += \
+    omni_charger_res_images
