bfe38ae9349ced158244450c245122eb8fd9f8e3
VimalRaj <vimalrajgks@gmail.com>
2017-07-15 13:38:51 +0530

    land: update tree

--- pa_land.mk
+++ pa_land.mk
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Define platform before including any common things
+$(call inherit-product, $(LOCAL_PATH)/PlatformConfig.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -23,12 +26,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from land device
-$(call inherit-product, device/xiaomi/land/device.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-PRODUCT_NAME := lineage_land
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := lineage_land
+
+
+#LETS BUILD WITH ROOT
+#WITH_SU := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
