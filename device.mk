92610f2cbdafc1ef669106533ed60bd05e55cec9
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:21 +0200

    land: Build Gatekeeper HAL

--- device.mk
+++ device.mk
@@ -180,16 +180,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(DEVICE_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf \
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(DEVICE_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(DEVICE_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+<<<<<<< HEAD:device.mk
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
 
 # IRQ
 PRODUCT_COPY_FILES += \
